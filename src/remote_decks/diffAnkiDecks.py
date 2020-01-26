ab6b6b46107bf794947ffcf2a59f96639a2a5244
c-okelly <okellyconor@gmail.com>
2020-01-25 23:14:43 -0800

    Add notes on deckDiff and update __init__

--- src/remote_decks/diffAnkiDecks.py
+++ src/remote_decks/diffAnkiDecks.py
@@ -50,6 +50,7 @@ def diffAnkiDecks(orgAnkiDeck, ankiBaseDeck):
                     questionsUpdated.append({"question":question, "noteId":noteId}) 
 
     # Find question in Anki that have been deleted from remote source
+    # TODO This is super slow
     for note in storedNotes:
         storedNote = storedNotes.get(note)
         keyField = _determineKeyField(storedNote)
