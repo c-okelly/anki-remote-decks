e06b6053c00b6d8b6cd60b4c2d0a50dad30eb89a
c-okelly <okellyconor@gmail.com>
2020-01-25 23:11:00 -0800

    Update shell scripts

--- localSwap.sh
+++ localSwap.sh
@@ -3,7 +3,10 @@ set -xe
 
 ankiHome=~/.local/share/Anki2/addons21/911568091
 
+rm release_*.zip
 ./package.sh
+
+rm $ankiHome/release_*.zip
 cp release_*.zip $ankiHome
 cd $ankiHome
 unzip -o release_*.zip
